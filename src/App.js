5f9f3797c8dd66b373b34dcbc997ddbc91c118e7
Jon Granados <grxnxdos@gmail.com>
2022-11-14 23:40:29 -0800

    Implemented route handling for Home and 404 errors.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import harvardArt from "./data/harvardArt"; 
 import GalleryNavigation from "./components/GalleryNavigation";
 import GalleryView from "./components/GalleryView";
-import { Route } from 'react-router-dom'; 
+import { Route, Switch } from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="page-wrapper">
       <GalleryNavigation galleries={harvardArt.records} />
 
-      <Route path="/galleries/:galleryId">
-        <GalleryView galleries={ harvardArt.records }/>
-      </Route>
+      <Switch>
+
+
+        <Route exact path="/">
+          <h2>Harvard Art Museum</h2>
+          <p>Look, but Don't Touch. Please select a Gallery in the navigation bar.</p>
+        </Route>
+
+        <Route path="/galleries/:galleryId">
+          <GalleryView galleries={harvardArt.records} />
+        </Route>
+
+        <Route>
+          <h2>Page not found.</h2>
+        </Route>
+
+      </Switch>
+
+
     </div>
 
     );
